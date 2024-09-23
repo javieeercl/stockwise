7309b958486f0cde48d199b7a4ac69e42b77c7fe
javieeercl <javieeercl@gmail.com>
2024-09-23 12:40:25 -0300

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyA_YFg-S-txsIf-pdeiinq5mqWfFTWieTk",
+    authDomain: "stockwise-fe332.firebaseapp.com",
+    projectId: "stockwise-fe332",
+    storageBucket: "stockwise-fe332.appspot.com",
+    messagingSenderId: "476222152603",
+    appId: "1:476222152603:web:81f1f0feb44c19c41e8b3e",
+    measurementId: "G-3D0953Z10M"
+  }
 };
