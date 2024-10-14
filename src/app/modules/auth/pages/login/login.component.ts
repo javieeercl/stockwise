46c0613d930ef336c1b2842151a233fe515119fd
javieeercl <javieeercl@gmail.com>
2024-10-13 22:11:14 -0300

    update login

--- src/app/modules/auth/pages/login/login.component.ts
+++ src/app/modules/auth/pages/login/login.component.ts
@@ -1,14 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { AuthService } from 'src/app/services/auth.service';
+import { InteractionsService } from 'src/app/services/interactions.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent  implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  form: FormGroup;
 
-  ngOnInit() {}
+  constructor(
+    public alertController: AlertController,
+    private auth: AuthService,
+    private interaction: InteractionsService,
+    private router: Router,
+    private fb: FormBuilder
+    )
+    {
+      this.form = this.fb.group({
+        email: ['', [Validators.email, Validators.required]],
+        password: ['', [Validators.required]],
+      });
+      this.form.reset();
+    }
 
+    get email(): any {
+      return this.form.get('email');
+    }
+    get password(): any {
+      return this.form.get('password');
+    }
+
+  async onSubmit(){
+    if(this.form.valid){
+      await this.interaction.showLoading('Iniciando Sesión...');
+      const { email, password } = this.form.value;
+
+      await this.auth.login(email, password).then((response) => {
+        this.interaction.closeLoading();
+        this.form.reset();
+        this.interaction.presentToast('Sesión Iniciada');
+        this.router.navigate(['/publico/portal']);
+      }).catch(err => {
+        this.interaction.closeLoading();
+        console.log(err);
+
+      });
+    }  else {
+      this.interaction.presentAlert('Formulario inválido', 'Datos incorrectos o incompletos', '', 'Ok');
+    }
+  }
 }
--- src/app/modules/auth/pages/recuperacion/recuperacion.component.ts
+++ src/app/modules/auth/pages/recuperacion/recuperacion.component.ts
@@ -1,3 +1,7 @@
+import { Router } from '@angular/router';
+import { AuthService } from './../../../../services/auth.service';
+import { InteractionsService } from './../../../../services/interactions.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -5,10 +9,45 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './recuperacion.component.html',
   styleUrls: ['./recuperacion.component.scss'],
 })
-export class RecuperacionComponent  implements OnInit {
+export class RecuperacionComponent {
 
-  constructor() { }
+  form: FormGroup;
 
-  ngOnInit() {}
+  constructor(
+    private interaction: InteractionsService,
+    private fb: FormBuilder,
+    private auth: AuthService,
+    private router: Router
+    )
+    {
+    this.form = this.fb.group({
+      email: ['', [Validators.email, Validators.required]]
+    });
+    this.form.reset();
+   }
 
+   get email(): any {
+    return this.form.get('email');
+  }
+
+
+  async onSubmit(){
+    if(this.form.valid){
+      const res = await this.interaction.presentAlert('Enviando Correo', 'Te enviaremos un correo de recuperación', '', 'Ok');
+        if (res) {
+          await this.interaction.showLoading('Enviando...');
+          const { email } = this.form.value;
+          await this.auth.requestPassword(email).then(() => {
+            this.form.reset();
+            this.interaction.closeLoading();
+            this.interaction.presentToast('Correo enviado con exito');
+            this.router.navigate(['/publico/login']);
+          }).catch(err => {
+            this.interaction.closeLoading();
+          });
+        }
+    }  else {
+      this.interaction.presentAlert('Formulario inválido', 'Datos incorrectos o incompletos', '', 'Ok');
+    }
+  }
 }
