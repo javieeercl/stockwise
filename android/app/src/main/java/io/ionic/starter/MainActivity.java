97a7167727a93f988667b74dbd5c8b89aff43119
javieeercl <javieeercl@gmail.com>
2024-12-16 18:36:33 -0300

    apk

---
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -0,0 +1,6 @@
+package com.alimentosfrescosdelsur.stockwise;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,31 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.alimentosfrescosdelsur.stockwise', // Cambia a tu ID correcto
   appName: 'stockwise',
-  webDir: 'www'
+  webDir: 'www',
+  bundledWebRuntime: false,
+
+  android: {
+    allowMixedContent: true, // Permitir contenido mixto (útil si tienes recursos HTTP y HTTPS)
+    backgroundColor: '#ffffff', // Color de fondo de la pantalla de carga en Android
+    webContentsDebuggingEnabled: true, // Habilitar depuración web para Android
+  },
+
+  server: {
+    cleartext: true, // Permite HTTP en lugar de HTTPS (solo para desarrollo)
+    androidScheme: 'https', // Usar 'https' para servir el contenido en Android
+  },
+
+  cordova: {
+    preferences: {
+      SplashMaintainAspectRatio: 'true',
+      FadeSplashScreenDuration: '300',
+      SplashShowOnlyFirstTime: 'false',
+      SplashScreen: 'screen',
+      SplashScreenDelay: '3000'
+    }
+  }
 };
 
 export default config;
